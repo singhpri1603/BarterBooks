f2423b98736eb4944d6809913f2a1d9a2f18b641
apschhokar <apschhokar@gmail.com>
2015-10-24 04:10:32 -0400

    UserProfileViewController Added

---
+++ BarterBooks/BarterBooksController/UserProfile/UserProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserProfileViewController.h
+//  BarterBooks
+//
+//  Created by ajay singh on 10/24/15.
+//  Copyright © 2015 UB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserProfileViewController : UIViewController
+
+@end
