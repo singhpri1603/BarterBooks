1237097153d4fbc9e49576ae9f0c8f6bacf0a85c
apschhokar <apschhokar@gmail.com>
2015-10-24 02:28:51 -0400

    Initial Commit

---
+++ BarterBooksTests/BarterBooksTests.m
@@ -0,0 +1,39 @@
+//
+//  BarterBooksTests.m
+//  BarterBooksTests
+//
+//  Created by ajay singh on 10/24/15.
+//  Copyright © 2015 UB. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BarterBooksTests : XCTestCase
+
+@end
+
+@implementation BarterBooksTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
