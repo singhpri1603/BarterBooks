1d06f358d2b89c2a4162dcee6f422f49ca5dfc44
apschhokar <apschhokar@gmail.com>
2015-11-02 23:15:08 -0500

    User registration page added

---
+++ BarterBooks/BarterBooksController/RegisterPage/RegisterPageViewController.h
@@ -0,0 +1,14 @@
+//
+//  RegisterPageViewController.h
+//  BarterBooks
+//
+//  Created by ajay singh on 10/27/15.
+//  Copyright © 2015 UB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RegisterPageViewController : UIViewController
+- (IBAction)onSubmitButtonClicked:(id)sender;
+
+@end
---
+++ BarterBooks/BarterBooksController/RegisterPage/RegisterPageViewController.m
@@ -0,0 +1,25 @@
+//
+//  RegisterPageViewController.m
+//  BarterBooks
+//
+//  Created by ajay singh on 10/27/15.
+//  Copyright © 2015 UB. All rights reserved.
+//
+
+#import "RegisterPageViewController.h"
+
+@implementation RegisterPageViewController
+
+
+-(void)viewDidAppear:(BOOL)animated{
+}
+
+-(void)viewDidLoad{
+}
+
+
+- (IBAction)onSubmitButtonClicked:(id)sender {
+}
+
+
+@end
