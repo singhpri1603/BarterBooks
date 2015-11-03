56b7c97cb5e3da501348e6cfa58b496115eaad58
apschhokar <apschhokar@gmail.com>
2015-11-03 00:44:16 -0500

    Added User session Model class

---
+++ BarterBooks/BarterBooksModel/UserSession.h
@@ -0,0 +1,19 @@
+//
+//  UserSession.h
+//  BarterBooks
+//
+//  Created by ajay singh on 11/3/15.
+//  Copyright © 2015 UB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserSession : NSObject
+
+
+@property NSString *UserName;
+@property NSString *UserID;
+@property int UserSession;
+
+
+@end
---
+++ BarterBooks/BarterBooksModel/UserSession.m
@@ -0,0 +1,17 @@
+//
+//  UserSession.m
+//  BarterBooks
+//
+//  Created by ajay singh on 11/3/15.
+//  Copyright © 2015 UB. All rights reserved.
+//
+
+#import "UserSession.h"
+
+
+
+@implementation UserSession
+
+
+
+@end
